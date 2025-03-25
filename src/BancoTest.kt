178eb937ba7a751b13dcd31fc50a33c1015bf9e3
Francisco Grande <grandefrancisco43@gmail.com>
2025-03-25 22:26:29 +0100

    cambios

--- src/BancoTest.kt
+++ src/BancoTest.kt
@@ -1,25 +1,68 @@
 
 
 import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
 class BancoTest {
 
+    private lateinit var cuenta: Banco
 
-
+    @BeforeAll
+    fun inicio() {
+        println("Este método se ejecuta antes de TODAS las pruebas")
+    }
 
     @BeforeEach
-    fun setup() {
+    fun presupuesto() {
+        cuenta = Banco(100.0) // Cada prueba comienza con un saldo de 100
         println("Este método se ejecuta antes de CADA prueba")
     }
 
     @Test
-    fun test1() {
-        println("Ejecutando test 1")
+    fun aumentosaldo() {
+        cuenta.deposito(50.0)
+        assertEquals(149.5, cuenta.getBalance())
     }
 
     @Test
-    fun test2() {
-        println("Ejecutando test 2")
+    fun retirosaldo() {
+        cuenta.retiro(50.0)
+        assertEquals(48.5, cuenta.getBalance())
+    }
+
+    @Test
+    fun retirosinsaldo() {
+        val exception = assertThrows<IllegalArgumentException> {
+            cuenta.retiro(200.0)
+        }
+        assertEquals("Fondos insuficientes", exception.message)
+    }
+
+    @Test
+    fun depositonegativo() {
+        val exception = assertThrows<IllegalArgumentException> {
+            cuenta.deposito(-10.0)
+        }
+        assertEquals("El depósito debe ser mayor que 0", exception.message)
+    }
+
+    @Test
+    fun retironegativo() {
+        val exception = assertThrows<IllegalArgumentException> {
+            cuenta.retiro(-10.0)
+        }
+        assertEquals("El retiro debe ser mayor que 0", exception.message)
+    }
+
+    @Test
+    fun tranferenciacuentas() {
+        val cuentaDestino = Banco(50.0)
+        cuenta.transferencia(cuentaDestino, 50.0)
+        assertEquals(48.5, cuenta.getBalance())
+        assertEquals(98.5, cuentaDestino.getBalance()) 
     }
 
     @AfterEach
@@ -30,10 +73,5 @@ class BancoTest {
     @AfterAll
     fun cleanupOnce() {
         println("Este método se ejecuta después de TODAS las pruebas")
-    }
-
-    @BeforeAll
-    fun setupOnce() {
-        println("Este método se ejecuta antes de TODAS las pruebas")
     }
 }
